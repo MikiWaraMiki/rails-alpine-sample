8f8966b1a514fd654be9b89e24487da39f5f216e
mikity_w <m_waragai@enfactory.co.jp>
2021-11-12 15:56:21 +0900

    wip

--- app/domain/model/route/route_table.rb
+++ app/domain/model/route/route_table.rb
@@ -2,7 +2,16 @@ class Model::Route::RouteTable
 
   def initialize()
     @table = [
-    ]
+      Model::Route::Route.new(
+        Model::Station::Station.new('tokyo'),
+        Model::Station::Station.new('shin_osaka')
+      )
+    ].freeze
+  end
+
+
+  def include?(route)
+    @table.include?(route) # obj == route
   end
 
   private
--- test/domain/model/route/route_table_spec.rb
+++ test/domain/model/route/route_table_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Model::Route::RouteTable, type: :model do
 
+  describe 'include?' do
+    context '存在する経路の場合' do
+      it 'trueを返す' do
+        table = Model::Route::RouteTable.new()
+
+        route = Model::Route::Route.new(
+          Model::Station::Station.new('tokyo'),
+          Model::Station::Station.new('shin_osaka')
+        )
+
+        expect(table.include?(route)).to be_truthy
+      end
+    end
+
+    context '存在しない経路の場合' do
+      it 'falseを返す' do
+        table = Model::Route::RouteTable.new()
+
+        route = Model::Route::Route.new(
+          Model::Station::Station.new('shin_osaka'),
+          Model::Station::Station.new('tokyo')
+        )
+
+        expect(table.include?(route)).to be_falsey
+      end
+    end
+  end
 end
